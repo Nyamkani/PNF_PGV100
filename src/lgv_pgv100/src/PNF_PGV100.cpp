948dc93ad2e593a0a4c75bfc07d95901647098ff
Kss <91517752+Nyamkani@users.noreply.github.com>
2022-04-24 00:05:55 +0900

    Add files via upload

--- src/lgv_pgv100/src/PNF_PGV100.cpp
+++ src/lgv_pgv100/src/PNF_PGV100.cpp
@@ -2,3 +2,28 @@
 #include <iostream>
 #include <sstream>
 #include <deque>
+
+//declare own ros2 headers
+#include "rclcpp/rclcpp.hpp"
+
+
+
+namespace
+{
+
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  auto sick_nav350_publisher = std::make_shared<sick_nav350>();
+  signal(SIGINT,ExitHandler);
+  int ret = sick_nav350_publisher->work_loop();
+
+  rclcpp::shutdown();
+  return ret;
+  //return 0;
+}
